50e60a373bc248a714c57931baad27c9c69ee8e5
Xavier <961588579@qq.com>
2017-05-13 14:52:01 +0800

    Revert "Revert "蕾姆小游戏""
    This reverts commit afa66d854cb558ce4a4f474c62b1c6c0ea026da7.

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.tag == "FFF") {
+			col.GetComponent<FFFController> ().Hurt ();
+		}
+	}
+}
---
+++ Assets/Scripts/FFFController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FFFController : MonoBehaviour
+{
+	public float rotationSpeed = 6;
+	public float moveSpeed = 6;
+	public GameObject blood;
+
+	Transform player;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = FFFManager.fm.player;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 targetDir = player.position - transform.position;
+		float step = rotationSpeed * Time.deltaTime;
+		Vector3 newDir = Vector3.RotateTowards (transform.forward, targetDir, step, 0.0f);
+		transform.rotation = Quaternion.LookRotation (newDir);
+
+		transform.Translate (Vector3.forward * Time.deltaTime * moveSpeed);
+	}
+
+	public void Hurt ()
+	{
+		Destroy (gameObject);
+		Instantiate (blood, transform.position, Quaternion.identity);
+	}
+}
