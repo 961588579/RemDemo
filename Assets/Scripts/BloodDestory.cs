682671b9c88a8479dec8fa5f12de20006c9cfead
Xavier <961588579@qq.com>
2017-05-13 14:37:05 +0800

    蕾姆小游戏

---
+++ Assets/Scripts/BloodDestory.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BloodDestory : MonoBehaviour
+{
+	public float delayTime = 5;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Destroy (gameObject, delayTime);
+	}
+
+}
+
---
+++ Assets/Scripts/RemController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemController : MonoBehaviour
+{
+
+	public float rotateSpeed = 120;
+	public float moveSpeed = 6;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position += transform.forward * Time.deltaTime * moveSpeed;
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate (Vector3.up * Time.deltaTime * -rotateSpeed);
+		} else if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate (Vector3.up * Time.deltaTime * rotateSpeed);
+		}
+	}
+}
